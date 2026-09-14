c7e4c2d4f175598670f7c15cb7b58b089efdf286
Joan <joantramadhan@gmail.com>
2026-09-14 11:46:15 +0700

    3 commit

--- index.js
+++ index.js
@@ -2,9 +2,13 @@ const express = require ('express');
 const app = express();
 const port = 3000;
 
+//import routes
+const moviesRouter = require('./routes/moives');
+
 //middleware untuk pharsing json
 app.use(express.json());
 
+app.use('/api/movies', moviesRouter);
 //route sederhana
 app.get('/', (req,res) => {
     res.send('hello world dari express')
---
+++ routes/moives.js
@@ -0,0 +1,51 @@
+const express = require('express');
+const router = express.Router();
+
+//data dummy
+let movies = [
+    {id : 1, title : 'inception', year : 2010},
+    {id : 2, title : 'interstellar', year : 2020},
+]
+
+// read all
+router.get('/', (req,res) => {
+    res.json(movies);
+});
+
+// read by id
+router.get('/:id', (req,res) =>{
+    const movie = movies.find(m => m.id === parseInt(req.params.id));
+    if(!movie) return res.status(404).json({massage: 'movie not found'});
+    res.json(movie);
+});
+
+//create
+router.post ('/', (req,res) =>{
+    const newMovie= {
+        id : movies.length + 1,
+        title : req.body.title,
+        yeaar : req.body.year
+    };
+    movie.pushnew(newMovie)
+    res.status(201).json(newMovie);
+});
+
+//edit /update
+router.put('/', (req,res) =>{
+    const movie = movies.find(m => m.id === parseInt(req.params.id));
+    if(!movie) return res.status(404).json({massage: 'movie not found'});
+
+    //else
+    movie.title = req.body.title || movie.title;
+    movie.year = req.body.year || movie.year;
+    res.json(movies);
+});
+
+//delete
+router.delete('/', (req,res) => {
+    movies = movies.filter(m => m.id !== parseInt(req.params.id));
+    res.status(204).end();
+});
+
+module.exports = router
+
